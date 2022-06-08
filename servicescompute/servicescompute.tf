0ebb4a162405992b288eec975edc88292e97746c
Robin Snelson <robinsnelson@robinmbp.local>
2022-06-08 18:24:34 +0100

    finished code up to compute being added

--- servicescompute/servicescompute.tf
+++ servicescompute/servicescompute.tf
@@ -12,7 +12,7 @@ resource "azurerm_network_interface" "servicescompute_vm1_interface" {
 }
 
 resource "azurerm_linux_virtual_machine" "servicescompute_vm1" {
-  name                = "${var.project_name}-hubcompute-vm1"
+  name                = "${var.project_name}-servicescompute-vm1"
   resource_group_name = azurerm_resource_group.servicescompute_rg.name
   location            = azurerm_resource_group.servicescompute_rg.location
   size                = "Standard_B2s"
